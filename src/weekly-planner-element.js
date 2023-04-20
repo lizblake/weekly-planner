36e886266a7bdf16c521a5f5557d29c00047bcda
Liz <elizabethblake@psu.edu>
2023-04-19 23:48:08 -0400

    Update weekly-planner-element.js

--- src/weekly-planner-element.js
+++ src/weekly-planner-element.js
@@ -4,7 +4,6 @@ import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 import "@lrnwebcomponents/a11y-collapse/a11y-collapse.js";
 import "@lrnwebcomponents/a11y-collapse/lib/a11y-collapse-group.js";
 
-
 class WeeklyPlannerElement extends LitElement {
   static properties = {
     plannerLabel: { type: String },
@@ -16,20 +15,28 @@ class WeeklyPlannerElement extends LitElement {
     plannerTimeCounter: { type: String },
     plannerObjectivesCounter: { type: String },
     plannerIconColor: { type: String },
-    openDetails: {type: Boolean, reflect: true},
+    openDetails: { type: Boolean, reflect: true },
     toggleText: { type: String },
   };
 
   static styles = css`
-  @import url('https://fonts.googleapis.com/css2?family=Anton&family=Source+Sans+Pro:wght@200&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Anton&family=Source+Sans+Pro:wght@200&display=swap");
 
     :host {
       font-size: 16px;
       font-weight: 100;
-      font-family: 'Anton', sans-serif;
-      font-family: 'Source Sans Pro', sans-serif;
-      --video-icon-background-color: linear-gradient(0.25turn,#FB814E,#F6B343);
-      --objectives-icon-background-color: linear-gradient(0.25turn, #45C9B4,#3BB3D5);
+      font-family: "Anton", sans-serif;
+      font-family: "Source Sans Pro", sans-serif;
+      --video-icon-background-color: linear-gradient(
+        0.25turn,
+        #fb814e,
+        #f6b343
+      );
+      --objectives-icon-background-color: linear-gradient(
+        0.25turn,
+        #45c9b4,
+        #3bb3d5
+      );
     }
 
     .plannerContainer {
@@ -93,7 +100,7 @@ class WeeklyPlannerElement extends LitElement {
       background: var(--objectives-icon-background-color);
     }
 
-    .videoLabel{
+    .videoLabel {
       font-weight: 400;
     }
     .objectivesLabel {
@@ -104,7 +111,6 @@ class WeeklyPlannerElement extends LitElement {
       margin: 0px 10px 0px 10px;
     }
 
-
     simple-icon {
       --simple-icon-color: white;
     }
@@ -113,7 +119,6 @@ class WeeklyPlannerElement extends LitElement {
       --a11y-collapse-border: none;
       --a11y-collapse-padding-left: 0px;
     }
-  
   `;
 
   constructor() {
@@ -121,12 +126,14 @@ class WeeklyPlannerElement extends LitElement {
     this.plannerLabel = "Week";
     this.plannerCounter = "1";
     this.plannerTitle = "Misconceptions about happiness";
-    this.plannerDescription = "In this module, you will learn what it means to be happy and why pursuing happiness is not a pointless endeavor. Dr. Santos addresses how our minds lie to us and how the science shows that our misconceptions about money, grades, and social media are holding us back from implementing the techniques studied in positive psychology.";
+    this.plannerDescription =
+      "In this module, you will learn what it means to be happy and why pursuing happiness is not a pointless endeavor. Dr. Santos addresses how our minds lie to us and how the science shows that our misconceptions about money, grades, and social media are holding us back from implementing the techniques studied in positive psychology.";
     this.plannerTimeIcon = "perm-identity";
     this.plannerObjectivesIcon = "perm-identity";
     this.plannerIconColor = "white";
     this.plannerTimeCounter = "2 hours to complete";
-    this.plannerObjectivesCounter = "9 videos (Total 55 min), 3 readings, 1 quiz";
+    this.plannerObjectivesCounter =
+      "9 videos (Total 55 min), 3 readings, 1 quiz";
     this.toggleText = "See all";
   }
 
@@ -140,41 +147,76 @@ class WeeklyPlannerElement extends LitElement {
   render() {
     return html`
       <div class="plannerContainer">
-        
-      <div class="plannerColumn left">
+        <div class="plannerColumn left">
           <div class="plannerLabel">${this.plannerLabel}</div>
           <div class="plannerCounter">${this.plannerCounter}</div>
         </div>
 
         <div class="plannerColumn right">
-          
+          <span class="iconContainer video">
+            <simple-icon
+              icon="${this.plannerTimeIcon}"
+              style="--simple-icon-color:${this.plannerIconColor};"
+            ></simple-icon
+          ></span>
 
-        <span class="iconContainer video">
-        <simple-icon
-            icon="${this.plannerTimeIcon}"
-            style="--simple-icon-color:${this.plannerIconColor};"
-          ></simple-icon></span>  
-          
           <span class="videoLabel spacing">${this.plannerTimeCounter}</span>
 
           <div class="plannerTitle">${this.plannerTitle}</div>
 
-          <div class="plannerDescription"><slot name="plannerText">${this.plannerDescription}</slot></div>
+          <div class="plannerDescription">
+            <slot name="plannerText">${this.plannerDescription}</slot>
+          </div>
 
           <a11y-collapse icon="none">
             <div slot="heading">
               <span class="iconContainer objectives">
-              <simple-icon style="--simple-icon-color:${this.plannerIconColor};"
-            icon="${this.plannerObjectivesIcon}"
-          ></simple-icon></span>
-          <span class="objectivesLabel spacing">${this.plannerObjectivesCounter}</span>
-          <span class="accordianToggle">${this.toggleText}</span>
+                <simple-icon
+                  style="--simple-icon-color:${this.plannerIconColor};"
+                  icon="${this.plannerObjectivesIcon}"
+                ></simple-icon
+              ></span>
+              <span class="objectivesLabel spacing"
+                >${this.plannerObjectivesCounter}</span
+              >
+              <span class="accordianToggle">${this.toggleText}</span>
+            </div>
+            <!-- make this a seperate element so I can just call that and modify it -->
+            <div class="videoSegment">
+              <h1>VIDEOS</h1>
+              Our Minds Are Bad at Predicting Our Feelings6m Happiness Problem
+              #1: We Get Used to Stuff but Don't Know It10m How Can We Deal with
+              Hedonic Adaptation?7m Happiness Problem #2: We Compare Ourselves a
+              Lot3m Happiness Problem #2: We Compare Ourselves to Other
+              People10m How Can We Deal with Social Comparisons?2m Happiness
+              Problem #3: Our Attention Is Limited7m How Can We Deal with
+              Limited Attention?5m
+            </div>
+            <div class="readingsSegment">
+            <h1>READINGS</h1>
+              Our Minds Are Bad at Predicting Our Feelings6m Happiness Problem
+              #1: We Get Used to Stuff but Don't Know It10m How Can We Deal with
+              Hedonic Adaptation?7m Happiness Problem #2: We Compare Ourselves a
+              Lot3m Happiness Problem #2: We Compare Ourselves to Other
+              People10m How Can We Deal with Social Comparisons?2m Happiness
+              Problem #3: Our Attention Is Limited7m How Can We Deal with
+              Limited Attention?5m
+            </div>
+            <div class="practiceSegment">
+            <h1>PRACTICE</h1>
+              Our Minds Are Bad at Predicting Our Feelings6m Happiness Problem
+              #1: We Get Used to Stuff but Don't Know It10m How Can We Deal with
+              Hedonic Adaptation?7m Happiness Problem #2: We Compare Ourselves a
+              Lot3m Happiness Problem #2: We Compare Ourselves to Other
+              People10m How Can We Deal with Social Comparisons?2m Happiness
+              Problem #3: Our Attention Is Limited7m How Can We Deal with
+              Limited Attention?5m
+            </div>
             </div>
           </a11y-collapse>
 
           <!-- When a11y-collapse is sent to expanded true, change See all to See less-->
         </div>
-      
       </div>
     `;
   }
