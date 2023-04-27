f2ec838ddefa6586c4a4b6861e79a435ff1add21
Liz <elizabethblake@psu.edu>
2023-04-27 09:38:48 -0400

    getting backend to work

--- src/content-list.js
+++ src/content-list.js
@@ -1,68 +1,76 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 import "./weekly-planner-element";
 
 export class ContentList extends LitElement {
-    static get tag() {
-        return 'content-list';
-    }
-    static get properties() {
-            return {
-                weeks: {type: Array},
-                month : {type: String}
-            }
-    }
+  static get tag() {
+    return "content-list";
+  }
+  static get properties() {
+    return {
+      weeks: { type: Array },
+      month: { type: String },
+    };
+  }
 
-    static get styles() {
-        return css`
-        :host {
-            display: block;
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      .wrapper {
+        border: 2px solid black;
+        display: flex;
+      }
+      .item {
+        display: inline-flex;
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.weeks = [];
+    this.month = "April";
+    this.updateWeek();
+  }
+
+  async updateWeek() {
+    const address = "../api/content";
+    fetch(address)
+      .then((response) => {
+        if (response.ok) {
+          console.log(response);
+          return response.json();
         }
-        .wrapper {
-            border: 2px solid black;
-            display: flex;
-        }
-        .item {
-            display: inline-flex;
-        }
-        `;
-    }
+        return [];
+      })
+      .then((data) => {
+        this.weeks = data;
+      });
+    console.log(data);
+  }
 
-
-    constructor() {
-        super();
-        this.weeks = [];
-        this.month = "April";
-        this.updateWeek(); 
-    }
-
-    async updateWeek() {
-      const address = new URL('../api/content.js', import.meta.url).href;
-      fetch(address).then((response) =>  {
-        if(response.ok) {
-            return response.json() 
-        }  
-        return[];
-        })
-        .then((data) => {
-            this.weeks = data;
-        }); 
-      console.log(data);
-
-    }
-
-    render() {
-        return html `
-        <h2>${this.month}</h2>
-            <div class="wrapper">
-                ${this.weeks.map((week) => 
-                    html `
-                    <div class="item">
-                        <weekly-planner-element weekCounter="${week.counter}" moduleTitle="${week.title}" moduleDescription="${week.description}" timeRemaining="${week.time}" objectivesTotal="${week.total}"></weekly-planner-element>
-                    </div>
-                `)}
-            </div>
-        `;
-    }
+  render() {
+    return html`
+      <h2>${this.month}</h2>
+      <div class="wrapper">
+        ${this.weeks.map(
+          (week) =>
+            html`
+              <div class="item">
+                <weekly-planner-element
+                  weekCounter="${week.counter}"
+                  moduleTitle="${week.title}"
+                  moduleDescription="${week.description}"
+                  timeRemaining="${week.time}"
+                  objectivesTotal="${week.total}"
+                ></weekly-planner-element>
+              </div>
+            `
+        )}
+      </div>
+    `;
+  }
 }
 
-customElements.define('content-list', ContentList);
+customElements.define("content-list", ContentList);
